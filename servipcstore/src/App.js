83902a7f145d58a00a63767f354bb30c63c29e36
papatelh <Hasan.Patel@tetrapak.com>
2023-08-20 19:30:24 -0500

    Initial Commit to ServiPC Store

---
+++ servipcstore/src/App.js
@@ -0,0 +1,66 @@
+
+
+const App = () => {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img /> */}
+        <div classname = "category-body-container">
+          <h2>Accesorios</h2>
+          <p>Ver Ahora</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div classname = "category-body-container">
+          <h2>Componentes</h2>
+          <p>Ver Ahora</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div classname = "category-body-container">
+          <h2>Computadoras</h2>
+          <p>Ver Ahora</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div classname = "category-body-container">
+          <h2>Monitores</h2>
+          <p>Ver Ahora</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div classname = "category-body-container">
+          <h2>Consumibles</h2>
+          <p>Ver Ahora</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div classname = "category-body-container">
+          <h2>Redes</h2>
+          <p>Ver Ahora</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div classname = "category-body-container">
+          <h2>Software</h2>
+          <p>Ver Ahora</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
+        <div classname = "category-body-container">
+          <h2>Servicios</h2>
+          <p>Ver Ahora</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;

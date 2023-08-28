7dec7d5f422e9f7ff5d3fa31cc16b77a111c7559
papatelh <Hasan.Patel@tetrapak.com>
2023-08-27 20:42:45 -0500

    Categories Component added

--- servipcstore/src/App.js
+++ servipcstore/src/App.js
@@ -1,66 +1,65 @@
+import './categories.styles.scss';
 
 
 const App = () => {
+
+  const categories = [
+    {
+      id : 1,
+      title: 'Accesorios',
+      imageUrl: 'https://www.tg-computers.co.uk/wp-content/uploads/2018/10/COMPUTER-accessories.jpg'
+      
+    },
+    {
+      id : 2,
+      title: 'Componentes',
+      imageUrl: 'https://i.pinimg.com/originals/3d/a2/7a/3da27a2f49d0a9e209bdbf719de9f571.jpg'
+      
+    },
+    {
+      id : 3,
+      title: 'Computadoras',
+
+      
+    },
+    {
+      id : 4,
+      title: 'Monitores',
+      
+    },
+    {
+      id : 5,
+      title: 'Consumibles',
+      
+    },
+    {
+      id : 6,
+      title: 'Redes',
+      
+    },
+    {
+      id : 7,
+      title: 'Software',
+      
+    },
+    {
+      id : 8,
+      title: 'Servicios',
+      
+    },
+  ]
   return (
     <div className="categories-container">
-      <div className="category-container">
-        {/* <img /> */}
-        <div classname = "category-body-container">
-          <h2>Accesorios</h2>
-          <p>Ver Ahora</p>
+      {categories.map(({title, id, imageUrl}) =>(
+        <div className="category-container">
+          <div className='background-image' />
+          <div className="category-body-container">
+          <h2>{title}</h2>
+          <p>Ver Mas</p>
         </div>
       </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div classname = "category-body-container">
-          <h2>Componentes</h2>
-          <p>Ver Ahora</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div classname = "category-body-container">
-          <h2>Computadoras</h2>
-          <p>Ver Ahora</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div classname = "category-body-container">
-          <h2>Monitores</h2>
-          <p>Ver Ahora</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div classname = "category-body-container">
-          <h2>Consumibles</h2>
-          <p>Ver Ahora</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div classname = "category-body-container">
-          <h2>Redes</h2>
-          <p>Ver Ahora</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div classname = "category-body-container">
-          <h2>Software</h2>
-          <p>Ver Ahora</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div classname = "category-body-container">
-          <h2>Servicios</h2>
-          <p>Ver Ahora</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
-
 export default App;
